344b177265a999f26875495df2a7357a5033a7bb
FLA <userFLA@example.com>
2024-12-28 12:52:07 +0100

    change color

--- extended.ts
+++ extended.ts
@@ -4,7 +4,7 @@ namespace ext_GamePad {
     //% blockId=ext_GamePad_RockerX block="RockerX"
     //% weight=96
     //% blockGap=10
-    //% color="#C814B8"
+    //% color="#0080FF"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=6
     export function RockerX() {
         return pins.analogReadPin(AnalogPin.P1);
@@ -13,7 +13,7 @@ namespace ext_GamePad {
     //% blockId=ext_GamePad_RockerY block="RockerY"
     //% weight=96
     //% blockGap=10
-    //% color="#C814B8"
+    //% color="#0080FF"
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=6
     export function RockerY() {
         return pins.analogReadPin(AnalogPin.P2);
