cf32ef64522af6568c19ed7150f6345a8b925939
FLA <userFLA@example.com>
2024-12-28 13:02:50 +0100

    m

--- extended.ts
+++ extended.ts
@@ -19,3 +19,31 @@ namespace ext_GamePad {
         return pins.analogReadPin(AnalogPin.P2);
     }
 }
+
+namespace ext_SuperBitV2 {
+    export enum enServo {
+        S1 = 0,
+        S2,
+        S3,
+        S4,
+        S5,
+        S6,
+        S7,
+        S8
+    }
+    
+    //% blockId=ext_SuperBitV2_Servo270 block="Servo(270°)|num %num|value %value"
+    //% weight=96
+    //% blockGap=10
+    //% color="#0080FF"
+    //% num.min=1 num.max=8 value.min=0 value.max=270
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=20
+    export function Servo270(num: enServo, value: number): void {
+        // Freq=50Hz => 20000 us => 4096 steps.
+        // GeekServo:
+        //   - min (0°)   =>  500 us =>  500*4096/20000 (=102).
+        //   - max (270°) => 2500 us => 2500*4096/20000 (=512).
+        let pwm = Math.map(value, 0, 270, 102, 512);
+        SuperBitV2.setPwm(num, 0, pwm);
+    }
+}
